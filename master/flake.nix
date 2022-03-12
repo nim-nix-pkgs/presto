799d7af9910365211cae271d3fd37868813f562d
hugosenari <hugosenari@gmail.com>
2022-03-12 04:04:23 -0300

    chore: re index presto

---
+++ master/flake.nix
@@ -0,0 +1,50 @@
+{
+  description = ''REST API framework for Nim language'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-presto-master.flake = false;
+  inputs.src-presto-master.owner = "status-im";
+  inputs.src-presto-master.ref   = "refs/heads/master";
+  inputs.src-presto-master.repo  = "nim-presto";
+  inputs.src-presto-master.type  = "github";
+  
+  inputs."chronos".dir   = "nimpkgs/c/chronos";
+  inputs."chronos".owner = "riinr";
+  inputs."chronos".ref   = "flake-pinning";
+  inputs."chronos".repo  = "flake-nimble";
+  inputs."chronos".type  = "github";
+  inputs."chronos".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."chronos".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."chronicles".dir   = "nimpkgs/c/chronicles";
+  inputs."chronicles".owner = "riinr";
+  inputs."chronicles".ref   = "flake-pinning";
+  inputs."chronicles".repo  = "flake-nimble";
+  inputs."chronicles".type  = "github";
+  inputs."chronicles".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."chronicles".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."stew".dir   = "nimpkgs/s/stew";
+  inputs."stew".owner = "riinr";
+  inputs."stew".ref   = "flake-pinning";
+  inputs."stew".repo  = "flake-nimble";
+  inputs."stew".type  = "github";
+  inputs."stew".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."stew".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-presto-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-presto-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
